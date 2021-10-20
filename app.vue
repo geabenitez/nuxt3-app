cbb090e1f584ea09d0fee68f98a6c4c51ca7e982
Gerson E. Aguirre <gerson@ap-techno.com>
2021-10-20 12:08:05 -0600

    FIRST COMMIT:

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-red-500 w-full font-semibold">
+    <span>Hola</span>
+    <el-row>
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
+  </div>
+</template>
